d47e408e711d12065bb7d468090322924c22ef07
Andy Eccleston <eandy5000@yahoo.com>
2017-02-03 18:56:08 -0600

    made basic reusable input component

---
+++ src/componenents/common/input.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const Input = ({ label, value, onChangeText, placeholder }) => {
+    const { inputStyle, labelStyle, viewStyle } = styles;
+    
+    return (
+        <View style={viewStyle}>
+            <Text style={labelStyle}>{label}</Text>
+            <TextInput
+                placeholder={placeholder} 
+                autoCorrect={false}
+                value={value}
+                onChangeText={onChangeText}
+                style={inputStyle}
+                // style={{ height: 20, width: 100 }}
+            />
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    labelStyle: {
+        fontSize: 18,
+        flex: 1,
+        paddingLeft: 20
+    },
+    inputStyle: {
+        color: '#000',
+        paddingLeft: 5,
+        paddingRight: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    }
+};
+
+export { Input };
--- src/componenents/login_form.js
+++ src/componenents/login_form.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+    state = {
+        text: ''
+    }
+    
     render() {
+        console.log(this.state.text);
         return (
             <Card>
                 <CardSection>
-                    <TextInput style={{ height: 20, width: 100 }} />
+                    <Input
+                        label='Email:'
+                        placeholder='user@company.com'
+                        onChangeText={text => this.setState({ text })} 
+                        value={this.state.text}
+                    />
                 </CardSection>
 
                 <CardSection />
